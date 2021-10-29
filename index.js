02bd6213d2e8457c94fbc848026bed395a1110db
Rahi776 <rahirahi9900@gmail.com>
2021-10-29 14:21:25 +0500

    this is final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello I am Node Server, hello this is john\n'
+  const msg = 'Hello I am Node Server, hello world hello this is john\n'
   res.end(msg);
 });
 
